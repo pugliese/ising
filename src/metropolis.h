97b566916843f1a36a57b5da1ba59b13b45a20fd
Pablo Alcain <pabloalcain@gmail.com>
2017-04-21 14:27:00 -0300

    Conflicting types

--- src/metropolis.h
+++ src/metropolis.h
@@ -2,5 +2,5 @@
 #define METROPOLIS_H
 int metropolis(int *lattice, int n, float T);
 int pick_site(int *lattice, int n);
-int flip(int *lattice, int n);
+int flip(int *lattice, int n, float T);
 #endif
