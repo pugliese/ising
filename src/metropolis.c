4a96818ad97c2e2c3b9a7cf38b0ef62c1624a991
pugliese <facu_pugliese@hotmail.com>
2017-05-09 16:01:56 -0300

    Algoritmos de metropolis.c listos

--- src/metropolis.c
+++ src/metropolis.c
@@ -1,13 +1,34 @@
 #include "metropolis.h"
 
 int metropolis(int *lattice, int n, float T) {
+  idx = pick_site(lattice, n);
+  flip(lattice, n, T, idx);
   return 0;
 }
 
 int pick_site(int *lattice, int n) {
-  return 0;
+  res = rand() % (n*n);
+  return res;
 }
 
-int flip(int *lattice, int n, float T) {
+int flip(int *lattice, int n, float T, int idx) {
+  int s = 0;
+  if (idx % n != n-1){
+    s = s + lattice[idx+1];
+  }
+  if (idx % n != 0){
+    s = s + lattice[idx-1];
+  }
+  if (idx/n !=0){
+    s = s+lattice[idx-n];
+  }
+  if (idx/n != n-1){
+    s = s+lattice[idx+n];
+  }
+  float deltaE = 2*lattice[idx]*s;
+  r = rand();
+  if(r<exp(-deltaE/T)){
+    lattice[idx] = -lattice[idx];
+  }
   return 0;
 }
