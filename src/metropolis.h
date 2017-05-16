3fdc01c630920b9d6e6236d6e63042c30e2c8b14
Gabriel Debais <gabriel.dbs.1@gmail.com>
2017-05-16 15:05:18 -0300

    faltaba el cambio del .h

--- src/metropolis.h
+++ src/metropolis.h
@@ -1,6 +1,6 @@
 #ifndef METROPOLIS_H
 #define METROPOLIS_H
-int metropolis(int *lattice, int n, float T);
+int metropolis(int *lattice, int n, float T, float *EM);
 int pick_site(int *lattice, int n);
-int flip(int *lattice, int n, float T, int idx);
+int flip(int *lattice, int n, float T, int idx, float *EM);
 #endif
