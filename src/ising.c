0ed910639df6b8f2944b736221bd212cc9e7e693
pugliese <facu_pugliese@hotmail.com>
2017-05-26 16:20:23 -0300

    Funcion para M(T) up&running

--- src/ising.c
+++ src/ising.c
@@ -10,6 +10,7 @@ int test_correlacion(int *lattice, int n, float B, float J, float* LUT, float *p
 int test_metropolis(int *lattice, int n, float B, float J, float* LUT, float* p_e, int* p_m);
 int test_vecinos(int n, int idx);
 int test_LUT(float* LUT);
+float magnet(int *lattice, int n, float p,float T_max, float T_min, int T_pasos,float B,float J, int niter,int k);
 
 int main(int argc, char **argv) {
   int n = 32;
@@ -21,15 +22,13 @@ int main(int argc, char **argv) {
   int M;
   int niter = 2000;
   float B = 0.1;
-  srand(1);
-  //srand(time(NULL));
-  float LUT[10];
-  for(int i=0;i<5;i++){ // Los posibles valores de los spins de alrededor son 2*i-4 para i=0,..,4 (-4,-2,0,2,4)
-    LUT[i] = exp(-(2*(J*(2*i-4)+B))/T);  // Spin positivo
-    LUT[i+5] = exp((2*(J*(2*i-4)+B))/T);  // Spin negativo
-  }
+  //srand(1);
+  srand(time(NULL));
+  float* LUT =LookUpTable(J,B,T);
   M=fill_lattice(lattice, n, prob);
-  E=energia_0(lattice,n,J,B);/*
+  E=energia_0(lattice,n,J,B);
+  magnet(lattice, n, prob, 3, 1.5, 501, 0, J, 1000,20000);
+  /*
   printf("E=%f\nM=%d\n", E,M);
 
   for (int i = 0; i < niter; i++) {
@@ -134,4 +133,36 @@ int test_LUT(float* LUT){
     printf("%f \n", LUT[i+5]);
   }
   return 0;
+}
+
+
+// LO DEJO ASI PORQUE PROBABLEMENTE NO COMPILE,
+// Me falta ver bien como poner la magnetizacipon osea
+// k cantidad de pasos que quiero que haga  hasta tomar m
+// niter la cantidad de ventanada de k pasos
+float magnet(int *lattice, int n, float p,float T_max, float T_min, int T_pasos,float B,float J, int niter,int k){
+  float Ti;
+  int M = fill_lattice(lattice,n,p);
+  float E = energia_0(lattice,n,J,B);
+  float* LUT;
+  FILE *fp = fopen("Magnetizacion.txt","a");
+  float T_step = (T_max-T_min)/(T_pasos-1);
+  fprintf(fp, "Magnetizacion para Temperaturas entre %g y %g con %d pasos \n ", T_max,T_min,T_pasos);
+  int secs = time(NULL);
+  for(Ti=T_min;Ti<=T_max;Ti=Ti+T_step){
+    float m_T=0;
+    LUT = LookUpTable(J,B,Ti);
+    for (int j = 0; j < niter; j++){
+      for(int l=0;l<k;l++){
+        metropolis(lattice, n,B,J,LUT,&E,&M);
+      }
+      m_T = m_T+((float)M)/niter;
+    }
+  fprintf(fp, "%g ,", m_T);
+  printf("%g finalizado\n", Ti);
+  free(LUT);
+  }
+  secs = time(NULL)-secs;
+  fprintf(fp, "\nEl calculo tomo %d hs, %d min, %d secs\n\n", secs/3600, (secs/60) % 60, secs % 60);
+  fclose(fp);
 }
