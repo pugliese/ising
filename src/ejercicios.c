fe5c1546ccdc7af9ee06e09f1371e7238ab7808e
Gabriel Debais <gabriel.dbs.1@gmail.com>
2017-05-30 16:42:10 -0300

    ejercicio2a

--- src/ejercicios.c
+++ src/ejercicios.c
@@ -5,3 +5,37 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+
+
+int ejercicio_2a(int *lattice, int n, float p,float T_max, float T_min, int T_pasos,float B, float J,int niter,int k){
+  float Ti;
+  float* LUT;
+  FILE *fp = fopen("Ejercicio2a.txt","a");
+  float T_step = (T_max-T_min)/(T_pasos-1);
+  fprintf(fp, "Energia para Temperaturas entre %g y %g con %d pasos \n ", T_max,T_min,T_pasos);
+  int secs = time(NULL);
+    for(Ti=T_min;Ti<=T_max;Ti=Ti+T_step){
+      fprintf(fp,"Temperatura %f \n", Ti );
+      printf("Temperatura %f\n", Ti);
+      float* m_e= malloc (k*sizeof(float));
+      for (int i=0;i<niter;i++){
+        int M = fill_lattice(lattice,n,p);
+        float E = energia_0(lattice,n,J,B);
+        LUT = LookUpTable(J,B,Ti);
+        for (int j = 0; j < k; j++){
+          metropolis(lattice, n,B,J,LUT,&E,&M);
+          m_e[j] = m_e[j] +((float) M)/niter;
+          }
+        }
+        for (int j=0; j<k ;j++){
+          fprintf(fp, "%g ,", m_e[j]);
+        }
+        fprintf(fp,"\n");
+        free(m_e);
+        free(LUT);
+      }
+  secs = time(NULL)-secs;
+  fprintf(fp, "\nEl calculo tomo %d hs, %d min, %d secs\n\n", secs/3600, (secs/60) % 60, secs % 60);
+  fclose(fp);
+  return 0;
+}
--- src/ejercicios.h
+++ src/ejercicios.h
@@ -1,4 +1,5 @@
 #ifndef EJERCICIOS_H
 #define EJERCICIOS_H
+int ejercicio_2a(int *lattice, int n, float p,float T_max, float T_min, int T_pasos,float B, float J,int niter,int k);
 
 #endif
